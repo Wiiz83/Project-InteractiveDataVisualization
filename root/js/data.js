d3b59d6edc71f27fcad07ed7a502ee05b7cb615c
Lucas <uzanlah@gmail.com>
2018-11-20 14:33:58 +0100

    mahdi est nul

--- root/js/data.js
+++ root/js/data.js
@@ -16,41 +16,41 @@ export function getCountryBubbles(geo, data, year = null) {
     borderOpacity: 1,
     radius: 0.4,
     fillOpacity: 0.75,
-    fillKey: "bubble",
+    fillKey: getColorFromKillNumber(u.nkill),
     borderColor: "#000000"
   }));
+}
+
+export function getColorFromKillNumber(mahdi) {
+  if(mahdi <= 10){
+    return "LOW";
+  } else if ((500 < mahdi) && (mahdi > 10)){
+    return "MEDIUM";
+  } else {
+    return "HIGH";
+  }
+
 }
 
 var dataset_events = [
   {
     name: "Hot",
-
     prop1: 45,
-
     latitude: 21.32,
-
     longitude: 5.32
   },
-
   {
     name: "Chilly",
     prop1: 45,
-
     latitude: -25.32,
-
     longitude: 120.32,
-
     radius: 1
   },
-
   {
     name: "Hot again",
     prop1: 45,
-
     latitude: 21.32,
-
     longitude: -84.32,
-
     radius: 1
   }
 ];
